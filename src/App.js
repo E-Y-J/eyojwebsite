693533af779c84b6b7083c99de1758740f6467b7
zzz <eyj.main@gmail.com>
2022-01-23 02:27:19 +0800

    cleanup
    clearing default

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hi! i am ellijah jimenez. <br/>
+      You can email me at <a href="mailto:info@ellijahjimenez.com">info@ellijahjimenez.com</a> <br />
     </div>
   );
 }
